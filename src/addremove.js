9cdd02dfb039c5bcc7bb5f6d7841756f28670b7b
Leandro <officialdevmi@gmail.com>
2021-07-21 10:15:30 -0700

    Add remove feature

--- src/addremove.js
+++ src/addremove.js
@@ -1,3 +1,6 @@
+import { dragStart, dragOver, drop } from './dropdrag';
+import { isComplete } from './completion';
+
 class SaveLocal {
   static saveArr(taskArr) {
     const acceptableString = JSON.stringify(taskArr);
@@ -17,8 +20,27 @@ class CreateTask {
   }
 }
 
+class RemoveTask {
+  static deleteBtn(btn, value, taskArr) {
+    btn.addEventListener('click', (ev) => {
+      const button = ev.target;
+      button.parentNode.parentNode.parentNode.removeChild(button.parentNode.parentNode);
+      this.outCollection(value, taskArr);
+    });
+  }
+
+  static outCollection(value, taskArr) {
+    const itemIndex = taskArr.indexOf(value);
+    taskArr.splice(itemIndex, 1);
+    for (let j = 0; j < taskArr.length; j += 1) {
+      taskArr[j].index = taskArr.indexOf(taskArr[j]);
+    }
+    SaveLocal.saveArr(taskArr);
+  }
+}
+
 class PopulateList {
-  static create(value) {
+  static create(value, taskArr) {
     const main = document.getElementById('sortList');
     const content = document.createElement('li');
     content.setAttribute('class', 'drag');
@@ -33,41 +55,88 @@ class PopulateList {
     const elem = document.createTextNode(`${value.description}`);
     cont.appendChild(elem);
     contentHolder.appendChild(cont);
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Delete';
+    btn.setAttribute('class', 'removeBtn');
     const icon = document.createElement('i');
     icon.setAttribute('class', 'fas fa-ellipsis-v');
     contentHolder.appendChild(icon);
+    contentHolder.appendChild(btn);
+    RemoveTask.deleteBtn(btn, value, taskArr);
     content.appendChild(contentHolder);
     main.appendChild(content);
   }
 }
 
-function addTask(taskArr, description, completed, index) {
-  const task = new CreateTask(description, completed, index);
+function addTask(taskArr) {
+  let desc = document.getElementById('data');
+  const task = new CreateTask(desc.value, 'false', taskArr.length);
   task.arrInsert(taskArr);
-  PopulateList.create(task);
+  PopulateList.create(task, taskArr);
+  SaveLocal.saveArr(taskArr);
 }
 
 export default class Preserve {
   static individualTasks(taskArr) {
     for (let i = 0; i < taskArr.length; i += 1) {
-      PopulateList.create(taskArr[i]);
+      PopulateList.create(taskArr[i], taskArr);
+    }
+
+    const items = document.querySelectorAll('#sortList li');
+
+    for (let i = 0; i < items.length; i += 1) {
+      items[i].addEventListener('dragstart', () => dragStart(items[i]));
+      items[i].addEventListener('dragover', (e) => dragOver(e));
+      items[i].addEventListener('drop', (e) => {
+        drop(e, items[i], taskArr);
+        SaveLocal.saveArr(taskArr);
+      });
+    }
+
+    const data = document.querySelectorAll('.data-box');
+
+    for (let j = 0; j < data.length; j += 1) {
+      data[j].addEventListener('change', () => {
+        isComplete(data[j], j, taskArr);
+        SaveLocal.saveArr(taskArr);
+      });
+
+      if (taskArr[j].completed === 'true') {
+        data[j].setAttribute('checked', '');
+        data[j].nextSibling.classList.add('done');
+      }
     }
   }
 
   static initialTask(taskArr) {
+    const form = document.getElementById('form');
     const input = document.querySelector('#data');
     const addBtn = document.querySelector('.fa-level-down-alt');
 
-    input.addEventListener('keyup', (e) => {
-      if (e.keyCode === 13) {
-        addBtn.click();
-      }
-    });
+    input.addEventListener('keypress', (e) => {
+      if (e.key === "Enter") {
+        addTask(taskArr);
+        form.reset();
 
-    addBtn.addEventListener('click', () => {
-      let desc = document.getElementById('data');
-      addTask(taskArr, desc.value, 'false', taskArr.length);
-      SaveLocal.saveArr(taskArr);
+        const items = document.querySelectorAll('#sortList li');
+
+        for (let i = 0; i < items.length; i += 1) {
+          items[i].addEventListener('dragstart', () => dragStart(items[i]));
+          items[i].addEventListener('dragover', (e) => dragOver(e));
+          items[i].addEventListener('drop', (e) => {
+            drop(e, items[i], taskArr);
+            SaveLocal.saveArr(taskArr);
+          });
+        }
+
+        const data = document.querySelectorAll('.data-box');
+        for (let j = 0; j < data.length; j += 1) {
+          data[j].addEventListener('change', () => {
+            isComplete(data[j], j, taskArr);
+            SaveLocal.saveArr(taskArr);
+          });
+        }
+      }
     });
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 import './reset.css';
 import './style.css';
-import { dragStart, dragOver, drop } from './dropdrag';
-import { isComplete } from './completion';
+//import { isComplete } from './completion';
+//import { dragStart, dragOver, drop } from './dropdrag';
 import Preserve from './addremove.js';
 
 const taskArr = JSON.parse(localStorage.getItem('myTasks')) || [];
@@ -16,32 +16,8 @@ class SaveLocal {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  Preserve.initialTask(taskArr);
   Preserve.individualTasks(taskArr);
-
-  const items = document.querySelectorAll('#sortList li');
-
-  for (let i = 0; i < items.length; i += 1) {
-    items[i].addEventListener('dragstart', () => dragStart(items[i]));
-    items[i].addEventListener('dragover', (e) => dragOver(e));
-    items[i].addEventListener('drop', (e) => {
-      drop(e, items[i], taskArr);
-      SaveLocal.saveArr();
-    });
-  }
-
-  const data = document.querySelectorAll('.data-box');
-  for (let j = 0; j < data.length; j += 1) {
-    data[j].addEventListener('change', () => {
-      isComplete(data[j], j, taskArr);
-      SaveLocal.saveArr();
-    });
-
-    if (taskArr[j].completed === 'true') {
-      data[j].setAttribute('checked', '');
-      data[j].nextSibling.classList.add('done');
-    }
-  }
+  Preserve.initialTask(taskArr);
 });
 
 /* eslint-enable max-classes-per-file */
