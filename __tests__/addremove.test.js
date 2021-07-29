cb2c397a9bdf396e0f4d393a66ba9fa654211d74
SamanAtashi <milady00126@gmail.com>
2021-07-29 11:53:00 +0300

    ADD test for local storage

--- __tests__/addremove.test.js
+++ __tests__/addremove.test.js
@@ -1,25 +1,34 @@
-import {addTask} from '../__mocks__/addremove.js';
-import {RemoveTask} from '../src/addremove.js';
+import { addTask } from '../__mocks__/addremove.js';
+import { RemoveTask } from '../src/addremove.js';
+import localStoragemock from '../__mocks__/local-storage-mocks';
 
 describe('add function', () => {
   test('add item to arr', () => {
-    let newArr = [];
-    document.body.innerHTML =
-    ' <div>' +
-    '   <ul id="sortList"></ul>' +
-    ' </div>';
+    const newArr = [];
+    document.body.innerHTML = ' <div>' + '   <ul id="sortList"></ul>' + ' </div>';
     addTask(newArr);
     expect(newArr.length).toBe(1);
-  })
+  });
 
   test('display element on the page', () => {
-    let newArr = [];
-    document.body.innerHTML =
-    ' <div>' +
-    '   <ul id="sortList"></ul>' +
-    ' </div>';
+    const newArr = [];
+    document.body.innerHTML = ' <div>' + '   <ul id="sortList"></ul>' + ' </div>';
     addTask(newArr);
     const list = document.querySelectorAll('#sortList li');
     expect(list).toHaveLength(1);
-  })
+  });
+
+  test('Add task to local Storage', () => {
+    const newObj = { description: 'sth', index: 1, completed: true };
+    localStoragemock.setItem('key', []);
+
+    const toDos = [];
+
+    toDos.push(newObj);
+
+    localStoragemock.setItem('key', toDos);
+
+    expect(localStoragemock.getItem('key')[0]).toEqual(newObj);
+    expect(localStoragemock.getItem('key').length).toEqual(1);
+  });
 });
