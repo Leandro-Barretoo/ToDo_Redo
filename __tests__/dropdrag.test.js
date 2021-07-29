3c3c71a257fd2056663100ee6b93a50d30791a1d
Leandro <officialdevmi@gmail.com>
2021-07-29 10:44:30 -0700

    Add test to check clear completed function

--- __tests__/dropdrag.test.js
+++ __tests__/dropdrag.test.js
@@ -1,4 +1,5 @@
 import {drop} from '../__mocks__/dropdrag.js';
+import {clearChecked} from '../__mocks__/addremove.js';
 
 describe('drag and drop', () => {
   test('update index', () => {
@@ -13,5 +14,24 @@ describe('drag and drop', () => {
     expect(arr).toEqual([{description: 'Task Two', completed: 'false', index: 0},
     {description: 'Task Three', completed: 'false', index: 1},
     {description: 'Task One', completed: 'false', index: 2}]);
+  })
+})
+
+describe('Completed status', () => {
+  test('get removed if true', () => {
+    const arr = [{description: 'Task One', completed: 'true', index: 0},
+    {description: 'Task Two', completed: false, index: 1}];
+    document.body.innerHTML =
+    '<div>' +
+    ' <ul id="sortList">' +
+    ' <li>Task One</li>' +
+    ' <li>Task Two</li>' +
+    ' </ul>' +
+    '</div>';
+
+    clearChecked(arr);
+    const items = document.querySelectorAll('#sortList li');
+    expect(items.length).toBe(1);
+    expect(arr).toHaveLength(1);
   })
 })
